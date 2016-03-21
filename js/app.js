bebef937477412378996b74966b717d2a1cc80a2
lsquach <lsquach@gmail.com>
2016-03-20 17:51:38 -0700

    updated game with styling

--- js/app.js
+++ js/app.js
@@ -7,12 +7,15 @@ $(document).ready(function() {
   var turn = 0;
   var icon = 'X';
   var player = 'Player 1';
+  var gameOn = true;
   //clicking event. Switch from X to O and draw the corresponding icon every turn, plus incrementing the turn counter
   $('.box').click(function handleTurn(){
-    if($(this).html()===""){
+    $('h2').text("Player 2 turn");
+    if($(this).html()==="" && gameOn === true){
       if (turn%2!==0) {
         icon = 'O';
         player = 'Player 2';
+        $('h2').text("Player 1 turn");
       } else {
         icon = 'X';
         player = 'Player 1';
@@ -20,46 +23,64 @@ $(document).ready(function() {
       $(this).html(icon);
       checkWin();
       turn++;
+      console.log(turn);
+    }
+    else if (gameOn === true){
+      alert("Pick another square!");
     }
     else {
-      alert("Pick another square!");
+      alert("Press Reset to start over");
+      $('h2').text(player + ' won!');
     }
   });
   //restart the game
   $('.btn').click(function handleReset(){
     $('.box').empty();
+    $('.box').removeClass('win');
+    $('h2').text("Player 1 goes first");
     turn = 0;
+    gameOn = true;
+
   });
   //check if any of the winning possibilities are true
   function checkWin(){
     if ($('#item1').html()===icon && $('#item2').html()===icon && $('#item3').html()===icon ) {
-      $('#item1, #item2, #item3');
-      alert(player + ' won!');
+      $('#item1, #item2, #item3').addClass('win');
+      $('h2').text(player + ' won!');
+      gameOn = false;
     } else if ($('#item4').html()===icon && $('#item5').html()===icon && $('#item6').html()===icon ) {
-      $('#item4, #item5, #item6');
-      alert(player + ' won!');
+      $('#item4, #item5, #item6').addClass('win');
+      $('h2').text(player + ' won!');
+      gameOn = false;
     } else if ($('#item7').html()===icon && $('#item8').html()===icon && $('#item9').html()===icon ) {
-      $('#item7, #item8, #item9');
-      alert(player + ' won!');
+      $('#item7, #item8, #item9').addClass('win');
+      $('h2').text(player + ' won!');
+      gameOn = false;
     } else if ($('#item7').html()===icon && $('#item4').html()===icon && $('#item1').html()===icon ) {
-      $('#item7, #item4, #item1');
-      alert(player + ' won!');
+      $('#item7, #item4, #item1').addClass('win');
+      $('h2').text(player + ' won!');
+      gameOn = false;
     } else if ($('#item8').html()===icon && $('#item5').html()===icon && $('#item2').html()===icon ) {
-      $('#item8, #item5, #item2');
-      alert(player + ' won!');
+      $('#item8, #item5, #item2').addClass('win');
+      $('h2').text(player + ' won!');
+      gameOn = false;
     } else if ($('#item9').html()===icon && $('#item6').html()===icon && $('#item3').html()===icon ) {
-      $('#item9, #item6, #item3');
-      alert(player + ' won!');
+      $('#item9, #item6, #item3').addClass('win');
+      $('h2').text(player + ' won!');
+      gameOn = false;
     } else if ($('#item7').html()===icon && $('#item5').html()===icon && $('#item3').html()===icon ) {
-      $('#item7, #item5, #item3');
-      alert(player + ' won!');
+      $('#item7, #item5, #item3').addClass('win');
+      $('h2').text(player + ' won!');
+      gameOn = false;
     } else if ($('#item9').html()===icon && $('#item5').html()===icon && $('#item1').html()===icon ) {
-      $('#item9, #item5, #item');
-      alert(player + ' won!');
+      $('#item9, #item5, #item1').addClass('win');
+      $('h2').text(player + ' won!');
+      gameOn = false;
     }
     //check if all the squares are taken
     else if (turn === 8) {
       alert("It's a draw!");
+      gameOn = false;
     }
   }
 });
